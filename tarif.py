3afffad64d8e8ef5f445c63b02e73c49899be84a
Shahrul Alfa Rizky <168643162+ISDPRODUCTION@users.noreply.github.com>
2025-10-08 12:06:10 +0700

    Update tarif.py

--- tarif.py
+++ tarif.py
@@ -7,7 +7,8 @@ elif (pemakaian <= 20):
     tarif = (10 * 5000) + (pemakaian - 10) * 7500
 elif (pemakaian <= 40):
     tarif = (10 * 5000) + (10 * 7500) + (pemakaian - 20) * 10000 
-else:  # pemakaian > 40
+elif (pemakaian > 40)
     tarif = (10 * 5000) + (10 * 7500) + (20 * 10000) + (pemakaian - 40) * 15000
+
 
 print("Total tagihan PDM adalah: Rp", f"{tarif:,.0f}")
